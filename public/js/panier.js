7e8b1467deb53dc0232821be3e6fe5c95bcd5218
Laure <quanelle@gmail.com>
2021-08-29 17:46:08 +0200

    Affichage de tous les éléments du panier
    changement div en tableau dans panier.html
    récupération et affichage de tous éléments du panier dans panier.js
    début mise en forme css panier

--- public/js/panier.js
+++ public/js/panier.js
@@ -1,16 +1,47 @@
-let articlePanier = document.querySelector('.article-panier');
-let nomArticlePanier = document.querySelector('.nom');
-let couleurArticlePanier = document.querySelector('.couleur');
-let prixArticlePanier = document.querySelector('.prix');
-console.log(prixArticlePanier);
 
-// récup produit stocké dans le localStorage et transformation du json en objet JS
+let tBody = document.querySelector('tBody');
+// console.log(tBody);
+
+
+// récup produit stocké dans le localStorage et transformation du json en objet JS(tableau)
 let produitPanier = JSON.parse(localStorage.getItem("produit"));
-// let produitPanierJson = localStorage.produit;
-// console.log(produitPanierJson);
-// let produitPanier = JSON.parse(produitPanierJson);
 console.log(produitPanier);
+
 // insertion des éléments dans le html du panier
-nomArticlePanier.textContent = produitPanier.nomProduit;
-couleurArticlePanier.textContent = produitPanier.choixCouleur;
-prixArticlePanier.textContent = produitPanier.prix+" €";
+
+
+for(i=0; i<produitPanier.length; i++){
+    // creation sous forme de variables des éléments à injecter dans le DOM
+   
+    let newArticle = document.createElement('tr');
+    let newName = document.createElement('td');
+    let newColor = document.createElement('td');
+    let newPrice = document.createElement('td');
+
+    // récupération de l'id
+    let id = produitPanier[i].idProduit;
+    console.log(id);
+   
+    // Données  à injecter dans les variables
+    newName.innerText =produitPanier[i].nomProduit;    
+    newPrice.innerText = produitPanier[i].prix+" euros";
+    newColor.innerText =produitPanier[i].choixCouleur;
+
+    // classes css dans variables
+    newArticle.classList = "article-panier";
+    newName.classList = "nom";
+    newPrice.classList = "prix";
+    newColor.classList = "couleur";
+    
+
+    // insertion des éléments dans les Div
+
+    newArticle.appendChild(newName);
+    newArticle.appendChild(newColor);
+    newArticle.appendChild(newPrice);
+    
+    
+    // ajout des Div dans liste
+    tBody.appendChild(newArticle);     
+    
+}
