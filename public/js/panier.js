9cb6c731429d8e29ac26143e0a79bd38b20e1e7b
Laure <quanelle@gmail.com>
2021-08-30 09:54:44 +0200

    Finalisation fonction total panier

--- public/js/panier.js
+++ public/js/panier.js
@@ -13,32 +13,32 @@ console.log(produitPanier);
 
 for(i=0; i<produitPanier.length; i++){
     // creation sous forme de variables des éléments à injecter dans le DOM
-   
+    
     let newArticle = document.createElement('tr');
     let newName = document.createElement('td');
     let newColor = document.createElement('td');
     let newPrice = document.createElement('td');
     
-
+    
     // récupération de l'id
     let id = produitPanier[i].idProduit;
     console.log(id);
-   
+    
     // Données  à injecter dans les variables
     newName.innerText =produitPanier[i].nomProduit;    
     newPrice.innerText = produitPanier[i].prix+" euros";
     newColor.innerText =produitPanier[i].choixCouleur;
-
-        
+    
+    
     // classes css dans variables
     newArticle.classList = "article-panier";
     newName.classList = "nom";
     newPrice.classList = "prix";
     newColor.classList = "couleur";
     
-
+    
     // insertion des éléments dans les Div
-
+    
     newArticle.appendChild(newName);
     newArticle.appendChild(newColor);
     newArticle.appendChild(newPrice);
@@ -46,24 +46,35 @@ for(i=0; i<produitPanier.length; i++){
     
     // ajout des Div dans liste
     tBody.appendChild(newArticle);   
-
-
-
     
-    // prix total du panier
-    let newPriceTotal = document.createElement('td');
+    
+    
+    
+}
+
+//----------------- prix total du panier--------------------------
+
+// creation sous forme de variables de l'élément à injecter dans le tfoot
+let newPriceTotal = document.createElement('td');
+// initialisation d'un tableau où seront listés les prix des articles du panier
+let TotalPrices=[];
+
+// boucle pour récupérer les prix du panier et en faire leur somme
+for(i=0; i<produitPanier.length; i++){
+    // récup de chaque prix du panier    
     let priceItem = produitPanier[i].prix;    
-    console.log(priceItem);
-    console.log(typeof priceItem);
-    
-    let Total=0;
-    console.log(typeof Total);
-    
-     Total+=priceItem;
-    
-   
-    console.log(Total);
+
+    // insertion de chaque prix dans le tableau TotalPrices  
+    TotalPrices.push(priceItem);
+    // console.log(TotalPrices);
+
+    // utilisation de la méthode reduce pour additionner toutes les valeurs du tableau TotalPrices    
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;   
+    let Total =TotalPrices.reduce(reducer);        
+    // console.log(Total);
+
+    // insertion du total dans le html
     newPriceTotal.innerText = Total+"€";
     somme.appendChild(newPriceTotal);  
 }
-
+    
